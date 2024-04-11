c397eb48c054c10981e51c55a1e9ef0a79618453
Лев Мухачев <163592011+LionMux@users.noreply.github.com>
2024-04-12 01:05:22 +0300

    Add files via upload

---
+++ Beginning.cs
@@ -0,0 +1,78 @@
+﻿public class Beginning
+{
+    float one;
+    float two;
+    float result;
+    char sign;
+    public void FirstN()
+    {
+        bool OneInput = false;
+
+        do
+        {
+            Console.Write("Введите первое число:");
+            if (float.TryParse(Console.ReadLine(), out one))
+            {
+                OneInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введненное число");
+            }
+        }
+        while (!OneInput);
+        }
+    public void Operation()
+    {
+        bool OpInput = false;
+        do
+        {
+            Console.Write("Введите оператор (+, -, *, /): ");
+            sign = Convert.ToChar(Console.ReadLine());
+            if (sign == '+' || sign == '-' || sign == '*' || sign == '/')
+            {
+                OpInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введенный оператор, вы можете ввести только: +, -, *, /");
+            }
+        }
+        while (!OpInput);
+    }
+    public void SecondN()
+    {
+        bool TwoInput = false;
+        do
+        {
+            Console.Write("Введи второе число: ");
+            if (float.TryParse(Console.ReadLine(), out two))
+            {
+                TwoInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введненное число");
+            }
+        }
+        while (!TwoInput);
+    }
+    public void Result()
+    {
+        result = sign == '+' ? one + two :
+                 sign == '-' ? one - two :
+                 sign == '*' ? one * two :
+                 sign == '/' && two != 0 ? one / two :
+                 throw new ArgumentException("Некорректный ввод");
+        string message = "";
+        if (sign == '/' && two == 0)
+        {
+            message = "Деление на ноль";
+        }
+        else
+        {
+            message = $"{one} {sign} {two} = {result}";
+        }
+        Console.WriteLine(message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Beginning menu = new Beginning();
+        Console.WriteLine("Добро пожаловать в мой калькуляктер 0_0");
+        Dictionary<string, string> actionMap = new Dictionary<string, string>
+        {
+            { "1", "Return" },
+            { "2", "Next" }
+        };
+        while (true)
+        {
+            menu.FirstN();
+            menu.Operation();
+            menu.SecondN();
+            menu.Result();
+            Console.WriteLine("\n1.Повторить\n2.Выход");
+            Console.Write("Выберите действие: ");
+            string userInput = Console.ReadLine();
+            if (actionMap.TryGetValue(userInput, out string selectedAction))
+            {
+                switch (selectedAction)
+                {
+                    case "Return":
+                        Console.Clear();
+                        break;
+                    case "Next":
+                        return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, выберите 1 (Return) или 2 (Next).");
+            }
+        }
+    }
+}
